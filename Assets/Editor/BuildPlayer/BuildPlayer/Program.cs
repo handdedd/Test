7fa8f85251a3fa2f89cb7214b319affaf5ced08b
DaeHoon <wkdwns11444@gmail.com>
2023-02-25 02:23:10 +0900

    빌드 자동화

---
+++ Assets/Editor/BuildPlayer/BuildPlayer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.Diagnostics;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayer : MonoBehaviour
+{
+    [MenuItem("Build/Build AOS")]
+    public static void MyBuild_AOS()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
+        buildPlayerOptions.locationPathName = $"Builds/AOS_{PlayerSettings.bundleVersion}.apk";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
